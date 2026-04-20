5f06d2db4ac0d49c06c08edd0e46f10c89217e80
Darwin <darwindavidrubio@gmail.com>
2026-04-20 18:53:45 -0500

    Cambiando la url a la de la pagina

--- usual.js
+++ usual.js
@@ -1,4 +1,4 @@
-const API_URL = "http://127.0.0.1:8000";
+const API_URL = "https://jeanasio.onrender.com";
 // "https://jeanasio.onrender.com"  cuando quiera cambiar algo del online
 // "http://127.0.0.1:8000" para el loqueri
 let paginaActual = 1;
